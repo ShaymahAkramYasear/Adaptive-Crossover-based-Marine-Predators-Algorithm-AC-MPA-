098cbdf002e36bcfdbdc5a14ba119a5d71933958
ShaymahAkram <shayma.akram.yasear@gmail.com>
2025-01-17 12:11:03 +0300

    Update Main_ACMPA.m

--- ACMPA_Code/Main_ACMPA.m
+++ ACMPA_Code/Main_ACMPA.m
@@ -1,3 +1,19 @@
+%% #############################################################################################
+% Adaptive-Crossover-based-Marine-Predators-Algorithm-AC-MPA-
+% This is the original code of Adaptive Crossover-based Marine Predators Algorithm (AC-MPA). 
+% The code was developed by Assist. Prof. Dr. Shaymah Akram Yasear.
+% 
+% The mathematical details of AC-MPA can be found in: Shaymah Akram Yasear, 
+% Adaptive crossover-based marine predators algorithm for global optimization problems, Journal of 
+% Computational Design and Engineering, Volume 11, Issue 4, August 2024, Pages 124–150, 
+% https://doi.org/10.1093/jcde/qwae060
+% 
+% Contact details of author: shayma.akram.yasear@gmail.com
+% https://www.researchgate.net/profile/Shaymah-Yasear
+% https://www.scopus.com/authid/detail.uri?authorId=57194554063
+% https://www.webofscience.com/wos/author/record/AAN-5105-2021
+%% #############################################################################################
+
 function []= Main_ACMPA()
     % Configuration parameters
     max_runs = 30;
